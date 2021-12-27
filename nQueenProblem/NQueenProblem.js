e6ed0ee0a65dba6284ae5e438ee5ec984be60293
Vivek <vivek.kumar@bjshomedelivery.com>
2021-12-27 19:03:36 +0530

    second commit

--- nQueenProblem/NQueenProblem.js
+++ nQueenProblem/NQueenProblem.js
@@ -1,17 +1,88 @@
-var NQueenProblem = function (boardSize) {
-    var board = [[], []];
-    for (var i = 0; i < boardSize; i++) {
-        for (var j = 0; j <= i; j++) {
-            if (i == 0 || j == 0)
-                board[i][j] = "hello";
-            board[i][j] = "hi";
-        }
-    }
-    for (var i = 0; i < boardSize; i++) {
-        for (var j = 0; j <= i; j++) {
-            console.log(board[i][j]);
-        }
+var Queen = function (boardSize) {
+    var board = [
+        ["0", "0", "0", "0"],
+        ["0", "0", "0", "0"],
+        ["0", "0", "0", "0"],
+        ["0", "0", "0", "0"],
+    ];
+    var newBoard = [
+        ["0", "0", "0", "0"],
+        ["0", "0", "0", "0"],
+        ["0", "0", "0", "0"],
+        ["0", "0", "0", "0"],
+    ];
+    for (var i = 0; i < 4; i++) {
+        QueenHelper(board, newBoard, i);
+        console.log("loop iteration " + i);
     }
 };
-var helper = function () { };
-NQueenProblem(4);
+//  saving the queen  state
+var saveBoard = function (board, newBoard) { };
+var QueenHelper = function (board, newBoard, positon) {
+    // base condtion
+    if (positon <= board.length) {
+        console.log("this is the board that i made " + board);
+        // saveBoard(board, newBoard);
+        return;
+    }
+    // loop threw the whole row
+    for (var j = 0; j < 4; j++) {
+        // check for isSafeToPlace queen using a  methodu
+        if (isSafeToPlace(board, positon, j)) {
+            board[positon][j] = "Q";
+            QueenHelper(board, newBoard, positon + 1);
+            board[positon][j] = "0";
+        }
+        console.log("its safe to place the qeen  ".concat(positon));
+    }
+    // if safe pace than palce the queen  and save the list
+};
+//  helper method for checking is the queen is safe to palce
+var isSafeToPlace = function (board, row, col) {
+    // horizontal check
+    for (var h = 0; h < board.length; h++) {
+        if (board[row][h] == "Q") {
+            return false;
+        }
+    }
+    // vertical check
+    for (var v = 0; v < board.length; v++) {
+        if (board[v][col] == "Q") {
+            return false;
+        }
+    }
+    // top left
+    var tempRow = row;
+    for (var tempCol = col; tempCol >= 0 && tempRow >= 0; tempCol--) {
+        if (board[tempRow][tempCol] == "Q") {
+            return false;
+        }
+        tempRow--;
+    }
+    //  top right
+    tempRow = row;
+    for (var tempCol = col; tempCol < board.length && tempRow >= 0; tempCol++) {
+        if (board[tempRow][tempCol] == "Q") {
+            return false;
+        }
+        tempRow--;
+    }
+    // bottom left
+    tempRow = row;
+    for (var tempCol = col; tempCol >= 0 && tempRow < board.length; tempCol--) {
+        if (board[tempRow][tempCol] == "Q") {
+            return false;
+        }
+        tempRow++;
+    }
+    //  bottom right
+    tempRow = row;
+    for (var tempCol = col; tempCol < board.length && tempRow < board.length; tempCol++) {
+        if (board[tempRow][tempCol] == "Q") {
+            return false;
+        }
+        tempRow++;
+    }
+};
+//  calling the function 
+Queen(4);
--- nQueenProblem/NQueenProblem.ts
+++ nQueenProblem/NQueenProblem.ts
@@ -1,25 +1,120 @@
-const NQueenProblem = (boardSize: number) => {
-  let board: string[][] = [[],[]];
-  for (let i = 0; i < boardSize; i++) {
-    for (let j = 0; j <= i; j++) {
-       
-       if(i == 0 || j == 0 )
-       board[i][j] = "hello";
-        board[i][j] = "hi";
-        
-    } 
-  }
+const Queen = (boardSize: number) => {
+  let board: string[][] = [
+    ["0", "0", "0", "0"],
+    ["0", "0", "0", "0"],
+    ["0", "0", "0", "0"],
+    ["0", "0", "0", "0"],
+  ];
 
+  let newBoard: string[][] = [
+    ["0", "0", "0", "0"],
+    ["0", "0", "0", "0"],
+    ["0", "0", "0", "0"],
+    ["0", "0", "0", "0"],
+  ];
 
-
-  for (let i = 0; i < boardSize; i++) {
-    for (let j = 0; j <= i; j++) {
-        console.log(board[i][j]);
-        
-    } 
+  for (let i: number = 0; i < 4; i++) {
+    QueenHelper(board, newBoard, i);
+    console.log("loop iteration "+i);
+    
   }
 };
 
-const helper = () => {};
 
-NQueenProblem(4);
+//  saving the queen  state
+const saveBoard = (board: string[][], newBoard: string[][]) => {};
+
+
+const QueenHelper = (
+  board: string[][],
+  newBoard: string[][],
+  positon: number
+) => {
+  // base condtion
+  if (positon <= board.length) {
+
+    console.log("this is the board that i made "+board);
+    
+    // saveBoard(board, newBoard);
+    return;
+  }
+  // loop threw the whole row
+
+  for (let j = 0; j < 4; j++) {
+
+  // check for isSafeToPlace queen using a  methodu
+    if (isSafeToPlace(board, positon, j)) {
+
+      
+      board[positon][j] = "Q";
+      QueenHelper(board, newBoard, positon + 1);
+      board[positon][j] = "0";
+    }
+
+    console.log(`its safe to place the qeen  ${positon}`);
+  }
+
+  // if safe pace than palce the queen  and save the list
+};
+
+//  helper method for checking is the queen is safe to palce
+const isSafeToPlace = (board: string[][], row: number, col: number) => {
+
+  // horizontal check
+  for (let h = 0; h < board.length; h++) {
+    if (board[row][h] == "Q") {
+      return false;
+    }
+  }
+
+
+  // vertical check
+ for (let v = 0; v < board.length; v++) {
+    if (board[v][col] == "Q") {
+      return false;
+    }
+  }
+
+  // top left
+  let tempRow = row;
+  for (let tempCol = col; tempCol >= 0 && tempRow >= 0; tempCol--) {
+    if (board[tempRow][tempCol] == "Q") {
+      return false;
+    }
+    tempRow--;
+  }
+
+  //  top right
+  tempRow = row;
+  for (let tempCol = col; tempCol < board.length && tempRow >= 0; tempCol++) {
+    if (board[tempRow][tempCol] == "Q") {
+      return false;
+    }
+    tempRow--;
+  }
+ 
+  // bottom left
+  tempRow = row;
+  for (
+    let tempCol = col;
+    tempCol >= 0 && tempRow < board.length;
+    tempCol--
+  ) {
+    if (board[tempRow][tempCol] == "Q") {
+      return false;
+    }
+    tempRow++;
+  }
+ 
+  //  bottom right
+  tempRow  = row
+  for (let tempCol = col; tempCol < board.length  && tempRow < board.length; tempCol++) {
+    if (board[tempRow][tempCol] == "Q") {
+      return false;
+    }
+    tempRow++;
+  }
+};
+//  calling the function 
+
+Queen(4);
