1d22233ed73624beaddd151e355ffacde144fc01
Viv3k <vivek.kumar@bjshomedelivery.com>
2022-07-23 16:53:28 +0530

    Leaving for now the longest palindrone question

--- out/production/Ds-Alog/leetcode/medium/LongestPalindromicSubstring.java
+++ out/production/Ds-Alog/leetcode/medium/LongestPalindromicSubstring.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class LongestPalindromicSubstring {
 
@@ -32,10 +29,64 @@ public class LongestPalindromicSubstring {
         // "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg";
         // System.out.println(isPalindrome(s, 0, s.length() - 1));
 
-        String s = "bacabab";
+        String s = "321012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210123210012321001232100123210123";
+
+        // String s = "racecar";
         // String s =
         // "321012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210123210012321001232100123210123";
-        System.out.println("OUtpu   --->   " + longestPalindrome(s));
+        // System.out.println("is Valid palindrome ->>>>" + isPalindrome(s, 0,
+        // s.length() - 1));
+
+        System.out.println(Palindrome(s));
+
+    }
+
+    public static String Palindrome(String s) {
+        String answer = "";
+        int maxLength = 0;
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        // for (int diff = 0; diff < n; diff++) {
+        // for (int i = 0, j = i + diff; j < n; i++, i++) {
+        // if (i == j) {
+        // dp[i][j] = 1;
+        // } else if (diff == 1) {
+        // dp[i][j] = (s.charAt(i) == s.charAt(j)) ? 2 : 0;
+        // } else {
+        // if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] != 0) {
+        // dp[i][j] = dp[i + 1][j - 1] + 2;
+        // }
+        // }
+
+        // if (dp[i][j] != 0) {
+        // if (j - i + 1 > maxLength) {
+        // maxLength = j - i + 1;
+        // answer = s.substring(i, maxLength);
+        // }
+        // }
+        // }
+        // }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] == 0) {
+                    dp[i][j] = dp[i][j] + 2;
+
+                }
+                if (dp[i][j] != 0) {
+                    if (j - i + 1 > maxLength) {
+                        maxLength = j - i + 1;
+                        answer = s.substring(i, maxLength);
+                    }
+                }
+
+            }
+        }
+
+        return answer;
+
     }
 
     public static String longestPalindrome(String s) {
@@ -45,7 +96,7 @@ public class LongestPalindromicSubstring {
         int n = s.length();
 
         for (int mid = 0; mid < n; mid++) {
-            for (int x = 1; mid - x >= 0 && mid + x < n; x++) {
+            for (int x = 0; mid - x >= 0 && mid + x < n; x++) {
                 if (s.charAt(mid - x) != s.charAt(mid + x)) {
                     break;
                 }
@@ -63,6 +114,10 @@ public class LongestPalindromicSubstring {
                     break;
                 }
                 int len = 2 * x;
+                if (len > bestLen) {
+                    bestLen = len;
+                    bestS = s.substring(mid - x + 1, len);
+                }
             }
         }
         return bestS;
