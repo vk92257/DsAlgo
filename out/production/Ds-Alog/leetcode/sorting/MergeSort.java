182b580ee9f3a9d4483616db19f74511b60307fc
Viv3k <vivek.kumar@bjshomedelivery.com>
2022-08-08 19:18:42 +0530

    Merge sort

--- out/production/Ds-Alog/leetcode/sorting/MergeSort.java
+++ out/production/Ds-Alog/leetcode/sorting/MergeSort.java
@@ -5,87 +5,86 @@ class MergeSort {
     public static void main(String[] args) {
         int[] input = { 1, 4, 5, 7, 3, 3 };
 
-        /*
-         * int startIndex = 0;
-         * int endIndex = /* input.length 0;
-         * int mid = (endIndex + startIndex) / 2;
-         * 
-         * int[] leftArray = Arrays.copyOfRange(input, 0, mid);
-         * int[] rightArray = Arrays.copyOfRange(input, mid, endIndex);
-         * 
-         * for (int j : leftArray) {
-         * System.out.println("left ARrray  ->>>>>>> " + j);
-         * }
-         * 
-         * for (int l : rightArray) {
-         * System.out.println("Right ARrray  ->>>>>>> " + l);
-         * }
-         */
+        // int startIndex = 0;
+        // int endIndex = input.length;
+        // int mid = endIndex / 2;
 
-        mergeSort(input, 0, input.length);
+        // int[] leftArray = Arrays.copyOfRange(input, 0, mid);
+        // int[] rightArray = Arrays.copyOfRange(input, mid, endIndex);
+
+        // for (int j : leftArray) {
+        // System.out.println("left ARrray ->>>>>>> " + j);
+        // }
+
+        // for (int l : rightArray) {
+        // System.out.println("Right ARrray ->>>>>>> " + l);
+        // }
+
+        mergeSort(input);
 
     }
 
-    private static void mergeSort(int[] input, int startIndex, int endIndex) {
-        if (input.length == 1) {
+    private static void mergeSort(int[] input) {
+        if (input.length <= 1) {
             return;
         }
 
-        int mid = (endIndex + startIndex) / 2;
+        int mid = input.length / 2;
 
-        int[] leftArray = Arrays.copyOfRange(input, startIndex, mid);
-        int[] rightArray = Arrays.copyOfRange(input, mid, endIndex);
+        int[] leftArray = Arrays.copyOfRange(input, 0, mid);
+        int[] rightArray = Arrays.copyOfRange(input, mid, input.length);
 
-        for (int j : leftArray) {
-            System.out.println(mid + "  = left ARrray  ->>>>>>> " + j);
-        }
+        /*
+         * for (int j : leftArray) {
+         * System.out.println(mid + "  = left ARrray  ->>>>>>> " + j);
+         * }
+         * 
+         * for (int l : rightArray) {
+         * System.out.println(mid + "   = Right ARrray  ->>>>>>> " + l);
+         * }
+         * for (int l : rightArray) {
+         * System.out.println(mid + "   = Right ARrray  ->>>>>>> " + l);
+         * }
+         */
 
-        for (int l : rightArray) {
-            System.out.println(mid + "   = Right ARrray  ->>>>>>> " + l);
-        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
 
-        mergeSort(leftArray, startIndex, mid);
-        mergeSort(rightArray, mid, endIndex);
-
-        merge(leftArray, rightArray);
+        merge(input, leftArray, rightArray);
 
     }
 
-    private static void merge(int[] l, int[] r) {
-
-        int sizeL = l.length;
-        int sizeR = r.length;
-        int[] output = new int[sizeL + sizeR];
+    private static void merge(int[] input, int[] l, int[] r) {
 
         int left = 0;
         int right = 0;
         int out = 0;
 
-        while (left < sizeL && right < sizeR) {
-            if (l[left] < r[right]) {
-                output[out] = r[right];
-                right++;
-            } else {
-                output[out] = l[left];
+        while (left < l.length && right < r.length) {
+            if (l[left] <= r[left]) {
+                input[out] = r[left];
                 left++;
+            } else {
+                input[out] = l[right];
+                right++;
             }
             out++;
         }
 
-        while (left < sizeL) {
-            output[out] = l[left];
+        while (left < l.length) {
+            input[out] = l[left];
             left++;
             out++;
         }
 
-        while (right < sizeR) {
-            output[out] = r[right];
+        while (right < r.length) {
+            input[out] = r[right];
             right++;
             out++;
         }
 
-        for (int i : output) {
-            System.out.println("left ARrray  ->>>>>>> " + i);
+        for (int i : input) {
+            System.out.print("-> " + i);
         }
 
     }
