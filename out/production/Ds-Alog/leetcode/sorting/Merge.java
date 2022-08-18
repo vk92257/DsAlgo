5f1abc0755f5107bc80e769fde6f030ac71de7a8
Viv3k <vivek.kumar@bjshomedelivery.com>
2022-08-18 20:20:00 +0530

    finding the largest second element

--- out/production/Ds-Alog/leetcode/sorting/Merge.java
+++ out/production/Ds-Alog/leetcode/sorting/Merge.java
@@ -2,15 +2,24 @@ import java.util.Arrays;
 
 public class Merge {
     public static void main(String[] args) {
-        int[] numbers = { 12, 12, 12, 12, 12 };
-        merge(numbers);
+        // int[] numbers = { 12, 12, 23, 4, 6, 6, 10, -35, 28 };
+        int[] numbers = { 2, 2, 2, 1 };
+        int[] highestNumbers = new int[2];
+        for (int i = 0; i < highestNumbers.length; i++) {
+            highestNumbers[i] = 0;
+        }
+        merge(numbers, highestNumbers);
         for (int i : numbers) {
             System.out.println(i);
         }
 
+        for (int i : highestNumbers) {
+            System.out.println("This is the code ->>>>> " + i);
+        }
+
     }
 
-    public static void merge(int[] numbers) {
+    public static void merge(int[] numbers, int[] highestNumbers) {
         if (numbers.length <= 1) {
             return;
         }
@@ -20,13 +29,13 @@ public class Merge {
         int[] arrayLeft = Arrays.copyOfRange(numbers, 0, mid);
         int[] arrayRight = Arrays.copyOfRange(numbers, mid, numbers.length);
 
-        merge(arrayLeft);
-        merge(arrayRight);
-        sort(arrayLeft, arrayRight, numbers);
+        merge(arrayLeft, highestNumbers);
+        merge(arrayRight, highestNumbers);
+        sort(arrayLeft, arrayRight, numbers, highestNumbers);
 
     }
 
-    public static void sort(int[] left, int[] right, int[] array) {
+    public static void sort(int[] left, int[] right, int[] array, int[] highestNumbers) {
 
         int l = 0;
         int r = 0;
@@ -36,9 +45,11 @@ public class Merge {
 
             if (left[l] <= right[r]) {
                 array[a] = left[l];
+                findHighest(highestNumbers, left[l]);
                 l++;
             } else {
                 array[a] = right[r];
+                findHighest(highestNumbers, right[r]);
                 r++;
             }
             a++;
@@ -47,6 +58,7 @@ public class Merge {
 
         while (l < left.length) {
             array[a] = left[l];
+            findHighest(highestNumbers, left[l]);
             l++;
             a++;
 
@@ -54,9 +66,30 @@ public class Merge {
 
         while (r < right.length) {
             array[a] = right[r];
+            findHighest(highestNumbers, right[r]);
             r++;
             a++;
 
+        }
+
+    }
+
+    public static void findHighest(int[] highestNumbers, int numbers) {
+        int temp = 0;
+
+        if (highestNumbers[0] < numbers) {
+            // if the highest numbes is small
+            temp = highestNumbers[0];
+            highestNumbers[0] = numbers;
+
+            if (highestNumbers[1] < temp) {
+                highestNumbers[1] = temp;
+            }
+
+        } else if (highestNumbers[1] < numbers && highestNumbers[0] > numbers) {
+            // if the second highest is small
+            temp = highestNumbers[1];
+            highestNumbers[1] = numbers;
         }
 
     }
